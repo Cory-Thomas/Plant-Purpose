f1da5b281e8beecc29e88dd851e28058c623e094
Cory Thomas <cory.a.thomas1992@gmail.com>
2021-02-24 15:23:20 -0500

    finished the Home page, and worked on some media queries for the page as well.

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { StyledDiv } from './styles/Home-styling';
-import { Leaf } from '@styled-icons/fa-solid/Leaf';
+import plantLogo from '../../assets/plantLogo.svg';
+import wateringCan from '../../assets/wateringCan.svg';
+import plant from '../../assets/plantFeatureLogo.svg';
+import plantNotes from '../../assets/plantNotes.svg';
+import plantPrimary from '../../assets/plantPrimary.svg';
+import vectorBackground from '../../assets/vectorBackground.svg';
+import facebook from '../../assets/facebook.svg';
+import twitter from '../../assets/twitter.svg';
+import instagram from '../../assets/instagram.svg';
+import plantFeature from '../../assets/plantFeature.svg';
 
 export const Home = () => {
   return (
     <StyledDiv>
       <header>
         <div className='logo'>
-          <Leaf />
+          <img src={plantLogo} alt=' ' />
           <h1>Plant Purpose</h1>
         </div>
+
         <nav>
           <Link to='/login'>
             <button className='signin'>Sign In</button>
@@ -20,23 +30,63 @@ export const Home = () => {
           </Link>
         </nav>
       </header>
+
       <main>
         <section>
-          <h2>Welcome to Plant Purpose</h2>
+          <h2>
+            Welcome to
+            <span>Plant Purpose</span>
+          </h2>
           <p>
             A plant mangement system, which helps plant owners take better care
             of their plants
           </p>
-          <button>Sign Up Now</button>
+          <Link to='/signup'>
+            <button>Sign Up Now</button>
+          </Link>
         </section>
-        <div>Image</div>
+
+        <div>
+          <img src={plantPrimary} alt=' ' className='plant' />
+          <img src={vectorBackground} alt=' ' />
+        </div>
       </main>
-      <section>
-        <div>Feature One</div>
-        <div>Feature Two</div>
-        <div>Feature Three</div>
+
+      <section className='features'>
+        <div>
+          <img src={plantFeature} alt=' ' className='plantFeature' />
+        </div>
+        <div>
+          <img src={wateringCan} alt=' ' />
+          <h3>Feature 1 Details</h3>
+          <p>Voluptate ullamco Voluptate ullamco Voluptate ullamco</p>
+        </div>
+        <div>
+          <img src={plant} alt=' ' />
+          <h3>Feature 2 Details</h3>
+          <p>Voluptate ullamco Voluptate ullamco Voluptate ullamco</p>
+        </div>
+        <div>
+          <img src={plantNotes} alt=' ' />
+          <h3>Feature 3 Details</h3>
+          <p>Voluptate ullamco Voluptate ullamco Voluptate ullamco</p>
+        </div>
       </section>
-      <footer>Footer</footer>
+
+      <footer>
+        <div>
+          <div>
+            <img src={facebook} alt=' ' />
+            <img src={twitter} alt=' ' />
+            <img src={instagram} alt=' ' />
+          </div>
+          <div>
+            <span>Privacy Policy</span>
+            <span>Terms of Use</span>
+            <span>Contact Us</span>
+          </div>
+        </div>
+      </footer>
     </StyledDiv>
   );
 };
--- src/components/pages/styles/Home-styling.js
+++ src/components/pages/styles/Home-styling.js
@@ -12,6 +12,15 @@ export const StyledDiv = styled.div`
     align-items: center;
 
     .logo {
+      width: 190px;
+      display: flex;
+      text-align: right;
+      font-size: 1.4rem;
+      line-height: 27px;
+      letter-spacing: 0.08em;
+      text-transform: capitalize;
+      color: var(--primary);
+
       svg {
         width: 40px;
       }
@@ -41,13 +50,168 @@ export const StyledDiv = styled.div`
     section {
       display: flex;
       flex-direction: column;
-      justify-content: space-around;
-      align-items: center;
+      justify-content: center;
+      align-items: flex-end;
+
+      h2 {
+        text-align: right;
+        font-size: 4.8rem;
+        line-height: 72px;
+        letter-spacing: 0.08em;
+        margin: 0;
+        margin-right: 95px;
+
+        span {
+          display: block;
+          color: var(--primary);
+        }
+      }
+
+      p {
+        width: 70%;
+        margin: 40px auto;
+        font-size: 1.8rem;
+        line-height: 27px;
+        text-align: right;
+        letter-spacing: 0.08em;
+      }
+
+      button {
+        width: 300px;
+        padding: 15px 0;
+        margin-right: 100px;
+        margin-bottom: 150px;
+      }
+    }
+
+    div {
+      position: relative;
+      display: inline;
+      margin-right: 30px;
+
+      .plant {
+        position: absolute;
+        bottom: 20;
+        right: 10;
+      }
     }
   }
 
-  section {
+  .features {
     display: flex;
     justify-content: space-around;
+    margin-top: 40px;
+    padding: 180px 0;
+    background-color: var(--background);
+
+    .plantFeature {
+      position: absolute;
+      width: 500px;
+      left: -40px;
+      padding-top: 100px;
+    }
+
+    div {
+      width: 260px;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      text-align: center;
+
+      img {
+        width: 100px;
+      }
+    }
+  }
+
+  footer {
+    display: flex;
+    justify-content: center;
+    padding-top: 300px;
+    margin-bottom: 80px;
+
+    div {
+      width: 350px;
+
+      div:first-of-type {
+        display: flex;
+        justify-content: space-around;
+        margin-bottom: 30px;
+      }
+
+      div:last-child {
+        display: flex;
+        justify-content: space-between;
+        color: var(--darktone);
+        letter-spacing: 0.08em;
+
+        span:nth-child(2) {
+          padding: 0 7px;
+          border-left: 1px solid var(--darktone);
+          border-right: 1px solid var(--darktone);
+        }
+      }
+    }
+  }
+
+  @media (max-width: 980px) {
+    header {
+      flex-direction: column;
+      margin-bottom: 80px;
+
+      nav {
+        width: 400px;
+        display: flex;
+        justify-content: space-around;
+        margin: 10px 0;
+
+        .signup {
+          margin-right: 0;
+        }
+      }
+    }
+
+    main {
+      flex-direction: column;
+
+      section {
+        align-items: center;
+
+        h2 {
+          margin: 0 auto;
+          margin-top: 40px;
+          text-align: center;
+        }
+
+        p {
+          text-align: center;
+        }
+
+        button {
+          margin: 10px auto;
+          margin-bottom: 40px;
+        }
+      }
+    }
+
+    .features {
+      flex-direction: column;
+      align-items: center;
+      padding: 60px 0;
+
+      .plantFeature {
+        display: none;
+      }
+
+      div {
+        width: 50%;
+        padding-bottom: 30px;
+      }
+    }
+
+    footer {
+      padding-top: 100px;
+    }
   }
 `;
